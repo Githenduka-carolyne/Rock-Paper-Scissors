2958c8ac50aafa42cf91ce8803e95b3c1a55a875
carolyne-wanjiru <shirogithenduka@gmail.com>
2024-05-26 16:15:06 +0300

    add the emoji changer for computer

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const compscores = document.getElementById("computerscores-display");
 const playerscores = document.getElementById("playerscores-display");
 const results = document.getElementById("result-details");
-const changerock = document.getElementById("rock")
-const changescissors = document.getElementById("scissors")
-const changepaper = document.getElementsByClassName("paper-emoji")
 const Choices = ["rock", "paper", "scissors"];
+const emojireplacer = document.getElementById("scissors");
+const emojireplacer2 = document.getElementById("rock");
 
 // let the scores start at 0
 let computerscores = 0;
 let playerscore = 0;
-let typeofemoji = ""
+
+
 
 // // function that allows one player to play the game
 // // give the choices indexed 0-rock 1-paper 2-scissors
 function playgame(playerchoice) {
     const computerchoice = Choices[Math.floor(Math.random()*3)];
+    
     
 
     console.log(computerchoice)
@@ -30,18 +31,19 @@ function playgame(playerchoice) {
         switch(playerchoice){
             case "rock":
                 result = (computerchoice === "scissors") ? "PLAYER WINS": "COMPUTER WINS"
-                typeofemoji = changerock
+               
                 break
             case "paper":
                 result = (computerchoice === "rock") ? "PLAYER WINS": "COMPUTER WINS"
-                typeofemoji = changescissors
+                
                 break
             case "scissors":
                 result = (computerchoice === "paper") ? "PLAYER WINS": "COMPUTER WINS"
-                typeofemoji = changepaper
+                
                 break            
         }
     }
+    // display results accordingly
     results.textContent = result;
     
     switch(result){
@@ -55,8 +57,30 @@ function playgame(playerchoice) {
             break; 
     } 
 
+    // replacing emojis with what the computer chooses
+    let computeremoji = ""
+
+    if (computerchoice === "rock") {
+        computeremoji=("✊" )  
+    }
+    else if(computerchoice === "paper"){
+        computeremoji=("✋")        
+        }
+    else{
+         computeremoji = ("✌️")
+    }
+    emojireplacer.textContent = computeremoji
+
+
+    //replacing emoji with what the player chooses 
+   
+  
+} 
+
+
+
     
-}  
+   
 
 
     
